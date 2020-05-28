8bf4f26f6b513e9cf4379e574f183018e52b9691
Armen <armproducer@gmail.com>
2020-05-28 14:31:30 +0300

    first_commit

---
+++ js/scroll.js
@@ -0,0 +1,22 @@
+$(".flowing-scroll").on("click", function () {
+  var el = $(this);
+  var dest = el.attr("href"); // получаем направление
+  if (dest !== undefined && dest !== "") {
+    // проверяем существование
+    $("html").animate(
+      {
+        scrollTop: $(dest).offset().top, // прокручиваем страницу к требуемому элементу
+      },
+      500 // скорость прокрутки
+    );
+  }
+  return false;
+});
+
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 1350) {
+    $(".pageup").fadeIn();
+  } else {
+    $(".pageup").fadeOut();
+  }
+});
---
+++ js/slider.js
@@ -0,0 +1,13 @@
+$(".main-carousel").flickity({
+  // options
+  cellAlign: "center",
+  contain: true,
+  wrapAround: true,
+});
+
+$(".reviews-carousel").flickity({
+  // options
+  cellAlign: "center",
+  contain: true,
+  wrapAround: true,
+});
