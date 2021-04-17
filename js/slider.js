cd6ca1f65d3799ed800129f8e71f0a27708f49a7
Armen <armproducer@gmail.com>
2021-04-17 16:46:16 +0300

    fix flickity finished

--- js/slider.js
+++ js/slider.js
@@ -1,18 +1,18 @@
 setTimeout(() => {
-  var elem = document.querySelector('.main-carousel');
-  var flkty = new Flickity( elem, {
+  const elem = document.querySelector('.main-carousel');
+  const flkty = new Flickity( elem, {
     cellAlign: "center",
     contain: true,
     wrapAround: true
   });
-  var elem2 = document.querySelector('.main-carousel2');
-  var flkty2 = new Flickity( elem2, {
+  const elem2 = document.querySelector('.main-carousel2');
+  const flkty2 = new Flickity( elem2, {
     cellAlign: "center",
     contain: true,
     wrapAround: true
   });
-  var elem3 = document.querySelector('.main-carousel3');
-  var flkty3 = new Flickity( elem3, {
+  const elem3 = document.querySelector('.main-carousel3');
+  const flkty3 = new Flickity( elem3, {
     cellAlign: "center",
     contain: true,
     wrapAround: true
@@ -26,14 +26,28 @@ $(".reviews-carousel").flickity({
   wrapAround: true,
 });
 
+const tabButtons = document.querySelectorAll('.tab_hide_button');
+const tabItems = document.querySelectorAll('.tab_hide_item');
 
-const tabButton = document.querySelector('.tab_hide_button');
-const tabItem = document.querySelector('.tab_hide_item');
-
-tabItem.style.visibility = 'hidden';
-tabItem.style.height = '0';
-
-tabButton.addEventListener('click', function() {
-  tabItem.style.visibility = 'visible';
-  tabItem.style.height = 'auto';
+tabItems.forEach( i => {
+  if(i.dataset.number !== '1') {
+    i.style.visibility = 'hidden';
+    i.style.height = '0';
+  }
 });
+
+tabButtons.forEach( b => {
+  b.addEventListener('click', function() {
+    tabItems.forEach( i => {
+      if(i.dataset.number === b.dataset.number) {
+        i.style.visibility = 'visible';
+        i.style.height = 'auto';
+      } else {
+        i.style.visibility = 'hidden';
+        i.style.height = '0';
+      }
+    })
+  });
+});
+
+
